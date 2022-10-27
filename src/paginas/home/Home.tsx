0235fe0f130f4927ae9ac8449af0a7270b99cab8
isabel-brolhato <isabel.brolhato@gmail.com>
2022-10-27 12:07:19 -0300

    Creates React app and adds Home page

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Home.css';
+
+function Home(){
+    return (
+        <div>
+<h1 className="titulo">Home</h1>
+<img className="img" src="https://images.unsplash.com/photo-1541701494587-cb58502866ab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="Imagem tela inicial">
+</img>
+</div>
+    )
+}
+
+export default Home;
