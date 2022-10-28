5058237c23ce4be009c342acbec87581b2e20350
isabel-brolhato <isabel.brolhato@gmail.com>
2022-10-28 11:41:00 -0300

    Adds styles for navbar, home and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 import Home from './paginas/home/Home';
+import Navbar from './components/estaticos/navbar/navbar';
+import Footer from './components/estaticos/footer/footer';
 
 function App() {
   return (
-    <Home />
+    <>
+    <Navbar/>
+    <Home/>
+    <Footer/>
+    </>
   );
 }
 
