6af23b65c594d762504992aba6a96f7ce74c1ea5
isabel-brolhato <isabel.brolhato@gmail.com>
2022-11-01 13:04:38 -0300

    Adds routes and modifies css

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './paginas/home/Home';
 import Navbar from './components/estaticos/navbar/navbar';
 import Footer from './components/estaticos/footer/footer';
+import Login from './paginas/login/Login';
 
 function App() {
   return (
-    <>
-    <Navbar/>
-    <Home/>
-    <Footer/>
-    </>
+    <Router>
+    <Navbar />
+    <Routes>
+
+      <Route path="/" element={<Login  />} />
+
+      <Route path="/home" element={<Home />} />
+
+      <Route path="/login" element={<Login />} />
+
+    </Routes>
+    <Footer />
+  </Router>
   );
 }
 
